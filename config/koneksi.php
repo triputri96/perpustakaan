b1b2cc1a6a016d3f640dd0ae2435cc78278f3e1b
putri <antariputri26@gmail.com>
2023-11-09 11:46:29 +0800

    update and delete cover buku

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,11 +2,7 @@
 // TODO: kalau gagal konek db, uncomment ni
 // $konek = mysqli_connect("localhost", "root", "");
 // TODO: comment ini
-<<<<<<< HEAD
 $konek = mysqli_connect("localhost", "root", "");
-=======
-$konek = mysqli_connect("localhost", "root", "cakratendados");
->>>>>>> b9be6bc154a7c77a49158fbb946f2187becfea51
 $db = mysqli_select_db($konek, "perpus");
 // if ($konek) {
 //     echo "berhasil";
--- pages/partials/footer.php
+++ pages/partials/footer.php
@@ -1,13 +1,6 @@
 <footer class="main-footer">
-<<<<<<< HEAD
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-=======
   <div class="float-right d-none d-sm-block">
     <b>Version</b> 3.2.0
   </div>
   <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
->>>>>>> b9be6bc154a7c77a49158fbb946f2187becfea51
 </footer>
