c61c943c22f13cbbc3cc2d0f8fb748e352f8b63d
Tini79 <artinichan79@gmail.com>
2023-11-09 21:30:35 +0700

    removed config

--- config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// TODO: kalau gagal konek db, uncomment ni
-// $konek = mysqli_connect("localhost", "root", "");
-// TODO: comment ini
-$konek = mysqli_connect("localhost", "root", "cakratendados");
-$db = mysqli_select_db($konek, "perpus");
-// if ($konek) {
-//     echo "berhasil";
-// }
