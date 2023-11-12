338bd1764f75919084694b4728af0353235d81a1
putri <antariputri26@gmail.com>
2023-11-12 19:22:52 +0800

    more

--- pages/admin/buku.php
+++ pages/admin/buku.php
@@ -2,59 +2,72 @@
 include('../partials/head.php');
 require('../../app/Services/buku.php');
 ?>
+<style>
+.hidden-content {
+    display: none;
+}
+
+.read-more-btn {
+    border: none;
+    background-color: transparent;
+    color: #808080;
+    cursor: pointer;
+}
+</style>
 
 <body class="hold-transition sidebar-mini">
-  <!-- Site wrapper -->
-  <div class="wrapper">
-    <!-- navbar -->
-    <?php include('../partials/navbar.php'); ?>
-    <!-- Main Sidebar Container -->
-    <?php include('../partials/aside.php'); ?>
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- navbar -->
+        <?php include('../partials/navbar.php'); ?>
+        <!-- Main Sidebar Container -->
+        <?php include('../partials/aside.php'); ?>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <?php include('../partials/breadcrumbs.php'); ?>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <?php include('../partials/breadcrumbs.php'); ?>
 
-      <!-- Main content -->
-      <section class="content">
+            <!-- Main content -->
+            <section class="content">
 
-        <!-- Default box -->
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Data Buku</h3>
-            <div class="card-tools">
-              <a class="btn btn-info" href="tambahBuku.php"><i class="fas fa-plus"></i> Insert</a>
-            </div>
-          </div>
-          <div class="card-body table-responsive p-0" style="height: 300px;">
-            <!-- Seach filter -->
-            <div class="d-flex justify-content-end search-container">
-              <form action="#search_results" method="get">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                  <div class="input-group-append">
-                    <button type="submit" name="table-btn-search" class="btn btn-default">
-                      <i class="fas fa-search"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <table class="table table-head-fixed text-nowrap">
-              <thead>
-                <tr>
-                  <th class="col-1">No</th>
-                  <th class="col-2">Judul</th>
-                  <th class="col-2">Pengarang</th>
-                  <th class="col-1">Genre</th>
-                  <th class="col-2">Deskripsi</th>
-                  <th class="col-2">Cover</th>
-                  <th class="col-2">Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
+                <!-- Default box -->
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Data Buku</h3>
+                        <div class="card-tools">
+                            <a class="btn btn-info" href="tambahBuku.php"><i class="fas fa-plus"></i> Insert</a>
+                        </div>
+                    </div>
+                    <div class="card-body table-responsive p-0" style="height: 400px;">
+                        <!-- Seach filter -->
+                        <div class="d-flex justify-content-end search-container">
+                            <form action="#search_results" method="get">
+                                <div class="input-group input-group-sm" style="width: 150px;">
+                                    <input type="text" name="table_search" class="form-control float-right"
+                                        placeholder="Search">
+                                    <div class="input-group-append">
+                                        <button type="submit" name="table-btn-search" class="btn btn-default">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <table class="table table-head-fixed text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="col-1">No</th>
+                                    <th class="col-2">Judul</th>
+                                    <th class="col-2">Pengarang</th>
+                                    <th class="col-1">Genre</th>
+                                    <th class="col-2">Deskripsi</th>
+                                    <th class="col-2">Cover</th>
+                                    <th class="col-2">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
                 if (isset($_GET['table_search'])) {
                   $cari = $_GET['table_search'];
                   $query = "SELECT * FROM data_buku WHERE judul LIKE '%$cari%' OR pengarang LIKE '%$cari%' OR genre LIKE '%$cari%' OR deskripsi LIKE '%$cari%'";
@@ -65,41 +78,69 @@ require('../../app/Services/buku.php');
                 $no = 1;
                 while ($book = mysqli_fetch_array($sql)) {
                 ?>
-                  <tr>
-                    <td><?php echo $no; ?></td>
-                    <td><?php echo $book['judul']; ?></td>
-                    <td><?php echo $book['pengarang']; ?></td>
-                    <td><?php echo $book['genre']; ?></td>
-                    <td><?php echo $book['deskripsi']; ?></td>
-                    <td><img src="../../dist/uploads/<?php echo $book['cover']; ?>" alt="gambar" style="width: 100px;"></td>
-                    <td class="d-flex">
-                      <a type="submit" name="editBuku" class="btn text-info" href="tambahBuku.php?editId=<?= $book['id'] ?>"><i class="fas fa-edit"></i></a>
-                      <form method="post" action="../../app/Services/buku.php?id=<?= $book['id']; ?>">
-                        <!-- <button type="submit" name="editBuku" class="btn text-info"><i class="fas fa-edit"></i></button> -->
-                        <button type="submit" name="deleteBuku" class="btn text-danger"><i class="fas fa-trash"></i></button>
-                      </form>
-                    </td>
-                  </tr>
-                <?php
+                                <tr>
+                                    <td><?php echo $no; ?></td>
+                                    <td><?php echo $book['judul']; ?></td>
+                                    <td><?php echo $book['pengarang']; ?></td>
+                                    <td><?php echo $book['genre']; ?></td>
+                                    <td class="col-2">
+                                        <div class="description-container">
+                                            <span
+                                                class="short-description"><?php echo substr($book['deskripsi'], 0, 10); ?>...</span>
+                                            <span
+                                                class="full-description hidden-content"><?php echo $book['deskripsi']; ?></span>
+                                            <button class="read-more-btn">More</button>
+                                        </div>
+                                    </td>
+                                    <td><img src="../../dist/uploads/<?php echo $book['cover']; ?>" alt="gambar"
+                                            style="width: 100px;"></td>
+                                    <td class="d-flex">
+                                        <a type="submit" name="editBuku" class="btn text-info"
+                                            href="tambahBuku.php?editId=<?= $book['id'] ?>"><i
+                                                class="fas fa-edit"></i></a>
+                                        <form method="post" action="../../app/Services/buku.php?id=<?= $book['id']; ?>">
+                                            <!-- <button type="submit" name="editBuku" class="btn text-info"><i class="fas fa-edit"></i></button> -->
+                                            <button type="submit" name="deleteBuku" class="btn text-danger"><i
+                                                    class="fas fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                <?php
                   $no++;
                 } ?>
-              </tbody>
-            </table>
-          </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.card -->
+
+            </section>
+            <!-- /.content -->
         </div>
-        <!-- /.card -->
+        <!-- /.content-wrapper -->
 
-      </section>
-      <!-- /.content -->
+        <?php include('../partials/footer.php'); ?>
+        <!-- Control Sidebar -->
+        <!-- <aside class="control-sidebar control-sidebar-dark"> -->
+        <!-- Control sidebar content goes here -->
+        <!-- </aside> -->
+        <!-- /.control-sidebar -->
     </div>
-    <!-- /.content-wrapper -->
+    <!-- ./wrapper -->
+    <?php include('../partials/scripts.php') ?>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        $(".read-more-btn").click(function() {
+            var container = $(this).parent();
+            container.find(".short-description, .full-description").toggleClass("hidden-content");
 
-    <?php include('../partials/footer.php'); ?>
-    <!-- Control Sidebar -->
-    <!-- <aside class="control-sidebar control-sidebar-dark"> -->
-    <!-- Control sidebar content goes here -->
-    <!-- </aside> -->
-    <!-- /.control-sidebar -->
-  </div>
-  <!-- ./wrapper -->
-  <?php include('../partials/scripts.php') ?>
+            if (container.find(".short-description").hasClass("hidden-content")) {
+                $(this).text("Less");
+            } else {
+                $(this).text("More");
+
+            }
+        });
+    });
+    </script>
--- pages/admin/user.php
+++ pages/admin/user.php
@@ -4,44 +4,45 @@ require('../../app/Services/user.php');
 ?>
 
 <body class="hold-transition sidebar-mini">
-  <div class="wrapper">
-    <?php include('../partials/navbar.php'); ?>
-    <?php include('../partials/aside.php'); ?>
-    <div class="content-wrapper">
-      <?php include('../partials/breadcrumbs.php'); ?>
-      <section class="content">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Data User</h3>
-            <div class="card-tools">
-              <a class="btn btn-info" href="tambahUser.php"><i class="fas fa-plus"></i> Insert</a>
-            </div>
-          </div>
-          <div class="card-body table-responsive p-0" style="height: 300px;">
-            <!-- Seach filter -->
-            <div class="d-flex justify-content-end search-container">
-              <form action="#search_results" method="get">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                  <div class="input-group-append">
-                    <button type="submit" name="table-btn-search" class="btn btn-default">
-                      <i class="fas fa-search"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <table class="table table-head-fixed text-nowrap">
-              <thead>
-                <tr>
-                  <th class="col-1">No</th>
-                  <th class="col-3">Username</th>
-                  <th class="col-3">Level</th>
-                  <th class="col-3">Aksi</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
+    <div class="wrapper">
+        <?php include('../partials/navbar.php'); ?>
+        <?php include('../partials/aside.php'); ?>
+        <div class="content-wrapper">
+            <?php include('../partials/breadcrumbs.php'); ?>
+            <section class="content">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Data User</h3>
+                        <div class="card-tools">
+                            <a class="btn btn-info" href="tambahUser.php"><i class="fas fa-plus"></i> Insert</a>
+                        </div>
+                    </div>
+                    <div class="card-body table-responsive p-0" style="height: 400px;">
+                        <!-- Seach filter -->
+                        <div class="d-flex justify-content-end search-container">
+                            <form action="#search_results" method="get">
+                                <div class="input-group input-group-sm" style="width: 150px;">
+                                    <input type="text" name="table_search" class="form-control float-right"
+                                        placeholder="Search">
+                                    <div class="input-group-append">
+                                        <button type="submit" name="table-btn-search" class="btn btn-default">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <table class="table table-head-fixed text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="col-1">No</th>
+                                    <th class="col-3">Username</th>
+                                    <th class="col-3">Level</th>
+                                    <th class="col-3">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
                 $username = $_SESSION['username'];
                 if (isset($_GET['table_search'])) {
                   $cari = $_GET['table_search'];
@@ -53,26 +54,27 @@ require('../../app/Services/user.php');
                 $no = 1;
                 while ($user = mysqli_fetch_array($sql)) {
                 ?>
-                  <tr>
-                    <td><?php echo $no; ?></td>
-                    <td><?php echo $user['username']; ?></td>
-                    <td><?php echo $user['level']; ?></td>
-                    <td>
-                      <form method="post" action="../../app/Services/user.php?id=<?= $user['id']; ?>">
-                        <!-- <button type="submit" name="edit" class="btn text-info"><i class="fas fa-edit"></i></button> -->
-                        <button type="submit" name="delete" class="btn text-danger"><i class="fas fa-trash"></i></button>
-                      </form>
-                    </td>
-                  </tr>
-                <?php
+                                <tr>
+                                    <td><?php echo $no; ?></td>
+                                    <td><?php echo $user['username']; ?></td>
+                                    <td><?php echo $user['level']; ?></td>
+                                    <td>
+                                        <form method="post" action="../../app/Services/user.php?id=<?= $user['id']; ?>">
+                                            <!-- <button type="submit" name="edit" class="btn text-info"><i class="fas fa-edit"></i></button> -->
+                                            <button type="submit" name="delete" class="btn text-danger"><i
+                                                    class="fas fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                <?php
                   $no++;
                 }
                 ?>
-              </tbody>
-            </table>
-          </div>
-          <!-- /.card-body -->
-          <!-- <div class="card-footer clearfix">
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                    <!-- <div class="card-footer clearfix">
             <ul class="pagination pagination-sm m-0 float-right">
               <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
               <li class="page-item"><a class="page-link" href="#">1</a></li>
@@ -81,10 +83,10 @@ require('../../app/Services/user.php');
               <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
             </ul>
           </div> -->
-          <!-- /.card-footer-->
+                    <!-- /.card-footer-->
+                </div>
+            </section>
         </div>
-      </section>
+        <?php include('../partials/footer.php'); ?>
     </div>
-    <?php include('../partials/footer.php'); ?>
-  </div>
-  <?php include('../partials/scripts.php') ?>
+    <?php include('../partials/scripts.php') ?>
